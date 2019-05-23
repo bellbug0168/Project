d23cef1d1e8821853f8b3451c752b91849c7d31e
hong <hong@naver.com>
2019-05-23 20:13:54 +0900

    icon2 img 추가 및 키보드로 조작 추가함

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ $(function () {
     $('#mslider').children('a').on('click', function () {
 
         if ($('#menu').hasClass('hide')) {
+
             $('#mslider').stop().animate({
                 'left': '235px'
             }, 300);
@@ -24,8 +25,10 @@ $(function () {
             setTimeout(function () {
                 $('#menu').css('z-index', '3');
             }, 300)
+            $('#mslider img').attr('src', 'img/icon2.png');
 
         } else {
+
             $('#menu').css('z-index', '-1');
             $('#cycle2').stop().animate({
                 'left': '0px'
@@ -40,11 +43,14 @@ $(function () {
                 $('#menu').hide();
 
             }, 300);
+            $('#mslider img').attr('src', 'img/icon.png');
 
         }
     });
     $('#menu li:eq(1)').on('click', function () {
+
         if (!($('#wrap').hasClass('mainhide'))) {
+            $('.keyhandler').removeClass('keyhandler');
             $('#menu').css('z-index', '-1');
             $('#cycle2').stop().animate({
                 'left': '0px'
@@ -88,7 +94,9 @@ $(function () {
         };
     });
     $('#menu li:eq(2)').on('click', function () {
+
         if (!($('#wrap').hasClass('mainhide'))) {
+            $('.keyhandler').removeClass('keyhandler');
             $('#menu').css('z-index', '-1');
             $('#cycle2').stop().animate({
                 'left': '0px'
@@ -112,15 +120,28 @@ $(function () {
             setTimeout(function () {
                 $('#section article:eq(0)').stop().slideDown(1000).removeClass('hide');
             }, 400);
+            for(let i=0; i<3;i++){
+                setTimeout(function(){
+                    $('#content >div').eq(i).animate({'left':'0'},2000);
+                })
+            }
+
         }else{
             $('#section article').not('.hide').stop().slideUp(500).addClass('hide');
             setTimeout(function () {
                 $('#section article:eq(0)').stop().slideDown(1000).removeClass('hide');
             }, 500);
+            for(let i=0; i<3;i++){
+                setTimeout(function(){
+                    $('#content >div').eq(i).animate({'left':'0'},2500);
+                })
+            }
         }
     });
     $('#menu li:eq(3)').on('click', function () {
+
         if (!($('#wrap').hasClass('mainhide'))) {
+            $('.keyhandler').removeClass('keyhandler');
             $('#menu').css('z-index', '-1');
             $('#cycle2').stop().animate({
                 'left': '0px'
@@ -152,7 +173,9 @@ $(function () {
         }
     });
     $('#menu li:eq(4)').on('click', function () {
+
         if (!($('#wrap').hasClass('mainhide'))) {
+            $('.keyhandler').removeClass('keyhandler');
             $('#menu').css('z-index', '-1');
             $('#cycle2').stop().animate({
                 'left': '0px'
@@ -183,4 +206,86 @@ $(function () {
             }, 500);
         }
     });
+    $('#menu nav li a').on("focus", function(){
+        $(this).parent().css('background-color', '#B2CCFF');
+        $('.keyhandler').removeClass('keyhandler');
+        $(this).addClass('keyhandler');
+    })
+    $('#menu nav li a').on("blur", function(){
+        $(this).parent().css('background-color', 'floralwhite');
+    })
+    $('#menu nav li').on("mouseover", function(){
+        $(this).css('background-color', '#B2CCFF');
+    })
+    $('#menu nav li').on("mouseleave", function(){
+        $(this).css('background-color', 'floralwhite');
+    })
+    $('#mslider a').on("focus",function(){
+        $('.keyhandler').removeClass('keyhandler');
+        $(this).addClass('keyhandler');
+    })
+    $(document).keydown(function(event){
+        var keyhandler = $('.keyhandler');
+        console.log(keyhandler);
+        switch(event.which){
+            case 40:
+                if($('#menu').hasClass('hide')){
+                    $('#mslider a').focus();
+                    console.log('menu focus');
+                }else{
+                     console.log('down');
+                    if($('#menu nav ul li a:eq(4)').hasClass('keyhandler')){
+                        $('#menu nav ul li a:eq(0)').focus();
+                    }else if($('#mslider a').hasClass('keyhandler')){
+                        console.log('main has key');
+                        $('#mslider a').focus();
+                    }else{
+                       console.log('dd') ;
+                        keyhandler.parent().next().children('a').focus();
+                    }
+                }
+                break;
+            case 37:
+                console.log('left');
+                if($('#menu').hasClass('hide')){
+                   $('#mslider a').focus();
+                    console.log('menu focus');
+                }else if($('#mslider a').hasClass('keyhandler')){
+                        console.log('focus');
+                        $('#menu nav ul li a:eq(0)').focus();
+
+                }else{
+
+                }
+                break;
+            case 39:
+                console.log('right');
+                if($('#menu').hasClass('hide')){
+                   $('#mslider a').focus();
+                    console.log('menu focus');
+                }else if($('#mslider a').hasClass('keyhandler')){
+                        console.log('focus');
+
+                }else{
+                    $('#mslider a').focus();
+                }
+                break;
+            case 38:
+                if($('#menu').hasClass('hide')){
+                    console.log('menu focus');
+                }else{
+                     console.log('up');
+                    if($('#menu nav ul li a:eq(0)').hasClass('keyhandler')){
+                        $('#menu nav ul li a:eq(4)').focus();
+                    }else if($('#mslider a').hasClass('keyhandler')){
+                        console.log('main has key');
+                        $('#mslider a').focus();
+                    }else{
+                       console.log('dd') ;
+                        keyhandler.parent().prev().children('a').focus();
+                    }
+                }
+                break;
+        }
+    })
 })
