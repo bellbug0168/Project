a26952db8d449d3f5c55a63b99639a1413f61f4d
hong <hong@naver.com>
2019-05-23 09:40:37 +0900

    .js파일을 수정합니다.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,45 @@
-$(function(){
-   $('#cycle').cycle({
-       fx: 'scrollLeft',
-       delay: -2000
-   });
-    $('#cycle2').cycle({
-       delay: -2000
-   });
+$(function () {
+    $(window).on("load resize", function () {
+        $('html').width($(this).width());
+        $('html').height($(this).height());
+        $('#cycle').height($(this).height());
+        $('#mslider').height($(this).height());
+        $('#cycle2').height($(this).height());
+        $('#cycle2move').height($(this).height());
+        $('#menu').height($(this).height());
+    })
 
-    $('#mslider').children('a').on('click', function(){
-        $('#mslider').stop().animate({'left': '47%'},500);
-        $('#cycle2move').stop().animate({'left': '54%'},500);
-        $('#menu').css('display', 'inline-block');
+    $('#cycle').cycle({
+        fx: 'scrollLeft',
+        delay: -2000
+    });
+    $('#cycle2').cycle({
+        delay: -2000
+    });
+
+    $('#mslider').children('a').on('click', function () {
+
+        if ($('#menu').hasClass('hide')) {
+            $('#mslider').stop().animate({
+                'left': '235px'
+            }, 500);
+            $('#cycle2').stop().animate({
+                'left': '235px'
+            }, 500);
+            $('#menu').show();
+            $('#menu').removeClass('hide');
+        }else{
+            $('#cycle2').stop().animate({
+                'left': '0px'
+            }, 500);
+            $('#mslider').stop().animate({
+                'left': '0px'
+            }, 500);
+            setTimeout(function(){
+                $('#menu').addClass('hide');
+                $('#menu').hide();
+            },500);
+
+        }
     })
 })
