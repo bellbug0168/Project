871da2143f7b1a8c6e2513fec27bc494db286bf1
hong <hong@naver.com>
2019-05-23 17:28:01 +0900

    2019/05/23 업데이트 합니다.

--- js/index.js
+++ js/index.js
@@ -66,12 +66,12 @@ $(function () {
                 $('#wrap').addClass('mainhide');
             }, 400);
             setTimeout(function () {
-                $('#section article:eq(2)').stop().slideDown(2000).removeClass('hide');
+                $('#section article:eq(2)').stop().slideDown(1000).removeClass('hide');
             }, 400);
         }else{
             $('#section article').not('.hide').stop().slideUp(500).addClass('hide');
             setTimeout(function () {
-                $('#section article:eq(1)').stop().slideDown(2000).removeClass('hide');
+                $('#section article:eq(1)').stop().slideDown(1000).removeClass('hide');
             }, 500);
         }
     });
@@ -110,12 +110,12 @@ $(function () {
                 $('#wrap').addClass('mainhide');
             }, 400);
             setTimeout(function () {
-                $('#section article:eq(0)').stop().slideDown(2000).removeClass('hide');
+                $('#section article:eq(0)').stop().slideDown(1000).removeClass('hide');
             }, 400);
         }else{
             $('#section article').not('.hide').stop().slideUp(500).addClass('hide');
             setTimeout(function () {
-                $('#section article:eq(1)').stop().slideDown(2000).removeClass('hide');
+                $('#section article:eq(0)').stop().slideDown(1000).removeClass('hide');
             }, 500);
         }
     });
@@ -142,14 +142,45 @@ $(function () {
                 $('#wrap').addClass('mainhide');
             }, 400);
             setTimeout(function () {
-                $('#section article:eq(1)').stop().slideDown(2000).removeClass('hide');
+                $('#section article:eq(1)').stop().slideDown(1000).removeClass('hide');
             }, 400);
         }else{
             $('#section article').not('.hide').stop().slideUp(500).addClass('hide');
             setTimeout(function () {
-                $('#section article:eq(1)').stop().slideDown(2000).removeClass('hide');
+                $('#section article:eq(1)').stop().slideDown(1000).removeClass('hide');
             }, 500);
         }
     });
+    $('#menu li:eq(4)').on('click', function () {
+        if (!($('#wrap').hasClass('mainhide'))) {
+            $('#menu').css('z-index', '-1');
+            $('#cycle2').stop().animate({
+                'left': '0px'
+            }, 300);
+            $('#mslider').stop().animate({
+                'left': '0px'
+            }, 300);
+            $('#mslider div').css('background-color', 'white');
+            setTimeout(function () {
+                $('#menu').addClass('hide');
 
+                $('#menu').hide();
+
+            }, 300);
+            setTimeout(function () {
+                $('#wrap').stop().animate({
+                    'left': '-47.5%'
+                }, 400);
+                $('#wrap').addClass('mainhide');
+            }, 400);
+            setTimeout(function () {
+                $('#section article:eq(3)').stop().slideDown(1000).removeClass('hide');
+            }, 400);
+        }else{
+            $('#section article').not('.hide').stop().slideUp(500).addClass('hide');
+            setTimeout(function () {
+                $('#section article:eq(3)').stop().slideDown(1000).removeClass('hide');
+            }, 500);
+        }
+    });
 })
