2224a896567baa073a64e16c41c83be97bb45d68
hong <hong@naver.com>
2019-05-23 11:44:15 +0900

    comiit 시행점 추가합니다.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $(function () {
+    setInterval(function(){
+        $('#cycle img').height($('body').height());
+        $('#cycle2 img').height($('body').height());
+    },100);
     $(window).on("load resize", function () {
         $('html').width($(this).width());
         $('html').height($(this).height());
-        $('#cycle').height($(this).height());
+        $('body').height($(this).height()); $('#cycle').height($(this).height());
         $('#mslider').height($(this).height());
         $('#cycle2').height($(this).height());
         $('#cycle2move').height($(this).height());
@@ -53,8 +57,25 @@ $(function () {
     });
     $('#menu li').eq(0).on('click',function(){
        if(!($('#wrap').hasClass('mainhide'))){
-           $('#wrap').stop().animate({'left':'-47.5%'},400);
+           $('#menu').css('z-index', '-1');
+            $('#cycle2').stop().animate({
+                'left': '0px'
+            }, 300);
+            $('#mslider').stop().animate({
+                'left': '0px'
+            }, 300);
+            $('#mslider div').css('background-color', 'dimgrey');
+            setTimeout(function () {
+                $('#menu').addClass('hide');
+
+                $('#menu').hide();
+
+            }, 300);
+           setTimeout(function(){
+               $('#wrap').stop().animate({'left':'-47.5%'},400);
            $('#wrap').addClass('mainhide');
+           },400);
+
        }
     });
     $('#menu li').eq(1).on('click',function(){
