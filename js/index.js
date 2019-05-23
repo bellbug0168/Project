e1c2ef32f17492f0486bcd5b22d77cd8f23e9b9e
hong <hong@naver.com>
2019-05-23 10:39:20 +0900

    왼쪽 당기기 animate를 추가합니다.

--- js/index.js
+++ js/index.js
@@ -22,24 +22,36 @@ $(function () {
         if ($('#menu').hasClass('hide')) {
             $('#mslider').stop().animate({
                 'left': '235px'
-            }, 500);
+            }, 300);
             $('#cycle2').stop().animate({
                 'left': '235px'
-            }, 500);
+            }, 300);
             $('#menu').show();
+            $('#mslider div').css('background-color', 'darkgrey');
             $('#menu').removeClass('hide');
-        }else{
+            setTimeout(function(){
+                $('#menu').css('z-index', '3');
+            },300)
+
+        } else {
+            $('#menu').css('z-index', '-1');
             $('#cycle2').stop().animate({
                 'left': '0px'
-            }, 500);
+            }, 300);
             $('#mslider').stop().animate({
                 'left': '0px'
-            }, 500);
-            setTimeout(function(){
+            }, 300);
+            $('#mslider div').css('background-color', 'dimgrey');
+            setTimeout(function () {
                 $('#menu').addClass('hide');
+
                 $('#menu').hide();
-            },500);
+
+            }, 300);
 
         }
-    })
+    });
+    $('#menu li').eq(0).on('click',function(){
+        $('#wrap').stop().animate({'left':'-47.5%'},400);
+    });
 })
