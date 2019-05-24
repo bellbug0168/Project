5ff66110e84c4cf4008a210c05930e0896cf8e9e
hong <hong@naver.com>
2019-05-24 16:25:14 +0900

    김정호 05/04 스크롤  기능을 추가합니다

--- js/pc_support_main.js
+++ js/pc_support_main.js
@@ -31,8 +31,68 @@ $(document).ready(function() {
             lastScrollTop = st;
         }
     //스크롤 기능 끝
+    // 전화 이벤트
+    $('.service_tel').on('mouseover', function(){
+        $('.service_tel_view').show();
+        $('.service_tel>p').hide();
+        $('.service_tel .tel').hide();
+    });
 
-    $('.service_tel a').on('mouseover focus', function(){
+    $('.service_tel a').on('focusin', function(){
+        $('.service_tel_view').show();
+        $('.service_tel>p').hide();
+        $('.service_tel .tel').hide();
+    });
 
-    })
+    $('.service_tel').on('mouseout', function(){
+        $('.service_tel_view').hide();
+        $('.service_tel>p').show();
+        $('.service_tel .tel').show();
+    });
+
+    $('.service_tel a').on('focusout', function(){
+        $('.service_tel_view').hide();
+        $('.service_tel>p').show();
+        $('.service_tel .tel').show();
+    });
+    // 전화이벤트 끝
+    // 이메일 이벤트
+    $('.service_email').on('mouseover', function(){
+        $('.service_email_view').show();
+        $('.service_email>p').hide();
+        $('.service_email .email').hide();
+    });
+
+    $('.service_email a').on('focusin', function(){
+        $('.service_email_view').show();
+        $('.service_email>p').hide();
+        $('.service_email .email').hide();
+    });
+
+    $('.service_email').on('mouseout', function(){
+        $('.service_email_view').hide();
+        $('.service_email>p').show();
+        $('.service_email .email').show();
+    });
+
+    $('.service_email a').on('focusout', function(){
+        $('.service_email_view').hide();
+        $('.service_email>p').show();
+        $('.service_email .email').show();
+    });
+    // 이메일 이벤트 끝
+    // 스크롤 위로
+    $(window).scroll(function() {
+	   if ($(this).scrollTop() > 120 ){
+		  $('.top').fadeIn();
+	   } else {
+		  $('.top').fadeOut();
+	   }
+    });
+
+    $('.top').click(function() {
+	   $('html, body').animate({scrollTop:0},400);
+	   return false;
+    });
+    // 스크롤 위로 끝
 });
