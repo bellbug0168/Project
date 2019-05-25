5aeb433fdd7df2dd9f6f102e8f8d74edad988c6c
hong <hong@naver.com>
2019-05-25 12:46:22 +0900

    김정호 05/25 자료를 추가합니다

--- js/support_main.js
+++ js/support_main.js
@@ -35,78 +35,91 @@ $(document).ready(function() {
             }
         lastScrollTop = st;
         }
-            $('.menu li').show();
         //스크롤 기능 끝
+
+            // 전화 이벤트
+        $('.service_tel').on('mouseover', function(){
+            $('.service_tel_view').show();
+            $('.service_tel>p').hide();
+            $('.service_tel .tel').hide();
+        });
+
+        $('.service_tel a').on('focusin', function(){
+            $('.service_tel_view').show();
+            $('.service_tel>p').hide();
+            $('.service_tel .tel').hide();
+        });
+
+        $('.service_tel').on('mouseout', function(){
+            $('.service_tel_view').hide();
+            $('.service_tel>p').show();
+            $('.service_tel .tel').show();
+        });
+
+        $('.service_tel a').on('focusout', function(){
+            $('.service_tel_view').hide();
+            $('.service_tel>p').show();
+            $('.service_tel .tel').show();
+        });
+        // 전화이벤트 끝
+
+        // 이메일 이벤트
+        $('.service_email').on('mouseover', function(){
+            $('.service_email_view').show();
+            $('.service_email>p').hide();
+            $('.service_email .email').hide();
+        });
+
+        $('.service_email a').on('focusin', function(){
+            $('.service_email_view').show();
+            $('.service_email>p').hide();
+            $('.service_email .email').hide();
+        });
+
+        $('.service_email').on('mouseout', function(){
+            $('.service_email_view').hide();
+            $('.service_email>p').show();
+            $('.service_email .email').show();
+        });
+
+        $('.service_email a').on('focusout', function(){
+            $('.service_email_view').hide();
+            $('.service_email>p').show();
+            $('.service_email .email').show();
+        });
+        // 이메일 이벤트 끝
+
+        $('.menu li').show();
         }
+
+
     });
 
 
     // width가 1023이하 일때
     $(window).on('resize load', function(){
         if($(window).width() < 1024) {
-
             $('.menu li').hide();
-
             $('.menu img').on('click', function(){
-                    $('.menu ul').toggle();
+                    $('.menu li').toggle();
             });
         }
 
     });
 
 
+    // width가 768이하 일때
+    $(window).on('resize load', function(){
+        if($(window).width() < 768) {
 
-
-    // 전화 이벤트
-    $('.service_tel').on('mouseover', function(){
-        $('.service_tel_view').show();
-        $('.service_tel>p').hide();
-        $('.service_tel .tel').hide();
+        }
     });
 
-    $('.service_tel a').on('focusin', function(){
-        $('.service_tel_view').show();
-        $('.service_tel>p').hide();
-        $('.service_tel .tel').hide();
-    });
 
-    $('.service_tel').on('mouseout', function(){
-        $('.service_tel_view').hide();
-        $('.service_tel>p').show();
-        $('.service_tel .tel').show();
-    });
 
-    $('.service_tel a').on('focusout', function(){
-        $('.service_tel_view').hide();
-        $('.service_tel>p').show();
-        $('.service_tel .tel').show();
-    });
-    // 전화이벤트 끝
-    // 이메일 이벤트
-    $('.service_email').on('mouseover', function(){
-        $('.service_email_view').show();
-        $('.service_email>p').hide();
-        $('.service_email .email').hide();
-    });
 
-    $('.service_email a').on('focusin', function(){
-        $('.service_email_view').show();
-        $('.service_email>p').hide();
-        $('.service_email .email').hide();
-    });
 
-    $('.service_email').on('mouseout', function(){
-        $('.service_email_view').hide();
-        $('.service_email>p').show();
-        $('.service_email .email').show();
-    });
 
-    $('.service_email a').on('focusout', function(){
-        $('.service_email_view').hide();
-        $('.service_email>p').show();
-        $('.service_email .email').show();
-    });
-    // 이메일 이벤트 끝
     // 스크롤 위로
     $(window).scroll(function() {
 	   if ($(this).scrollTop() > 120 ){
@@ -121,4 +134,5 @@ $(document).ready(function() {
 	   return false;
     });
     // 스크롤 위로 끝
+
 });
