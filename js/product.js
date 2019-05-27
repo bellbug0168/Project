92300343a674c27949c8553cc81f550164deadf4
muring <the_tassadar@naver.com>
2019-05-27 20:27:26 +0900

    자바 스크립트를 수정합니다.

--- js/product.js
+++ js/product.js
@@ -24,29 +24,41 @@ jQuery(document).ready(function ($) {
     var c = $('.header_nav').find('ul'); // 메뉴들
 
     b.on('mouseover', function () {
-        b.fadeOut(2000, function() {
-            b.fadeIn(2000, function() {
+        b.fadeOut(2000, function () {
+            b.fadeIn(2000, function () {
 
             });
         });
         // c.appendTo(this).css('background-color', 'lime')
     });
 
+    // menu slide down
+    var slide = $(".header_nav").find('ul').find('li').eq(0);
+    slide.on('click', function () {
+        $(this)
 
-    // mouseover
+        slide.slideDown();
+    });
+
+
+    // 필터링
+    var ar_fil = $('article').find('.pronum');
+    console.log(ar_fil);
+
+    // mouseover 시 article 사진 확대
     var d = $('article').find('ul').find('li').find('a').find('img');
     d.on('mouseover', function () {
-        d.eq(0).css('transform', 'scale(1.5)');
+        $(this).css('transform', 'scale(1.5)');
     });
     d.on('mouseout', function () {
-        d.eq(0).css('transform', 'scale(1)');
+        $(this).css('transform', 'scale(1)');
     });
 
     // focus, blur tab 시 배경색 변경
-    $('a').on('focus', function() {
+    $('a').on('focus', function () {
         $(this).css('background-color', 'chartreuse');
     });
-    $('a').on('blur', function() {
+    $('a').on('blur', function () {
         $(this).css('background-color', 'white');
-    })
+    });
 });
