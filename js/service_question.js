d1754fac9042cb5905169c36556288687b1c685c
hong <hong@naver.com>
2019-05-27 11:03:58 +0900

    김정호 05/27 모바일 페이지를 추가합니다

--- js/service_question.js
+++ js/service_question.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
 
+
+    //스크롤 이동 시 메뉴 on/off
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 100;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight){
+            $('nav').removeClass('nav-down').addClass('nav-up');
+        } else {
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+    lastScrollTop = st;
+    }
+    //스크롤 기능 끝
+
     // 스크롤 위로
     $(window).scroll(function() {
 	   if ($(this).scrollTop() > 120 ){
@@ -29,46 +64,13 @@ $(document).ready(function(){
     });
     // 스크롤 센터위치로 끝
 
+
     // width가 1024 이상일떄
     $(window).on('resize load', function(){
         if($(window).width() > 1023) {
-            //스크롤 이동 시 메뉴 on/off
-            var didScroll;
-            var lastScrollTop = 0;
-            var delta = 100;
-            var navbarHeight = $('header').outerHeight();
-
-        $(window).scroll(function(event){
-            didScroll = true;
-        });
-
-        setInterval(function() {
-            if (didScroll) {
-                hasScrolled();
-                didScroll = false;
-            }
-        }, 250);
-
-        function hasScrolled() {
-            var st = $(this).scrollTop();
-
-            if(Math.abs(lastScrollTop - st) <= delta)
-                return;
-
-            if (st > lastScrollTop && st > navbarHeight){
-                $('nav').removeClass('nav-down').addClass('nav-up');
-            } else {
-                if(st + $(window).height() < $(document).height()) {
-                    $('nav').removeClass('nav-up').addClass('nav-down');
-                }
-            }
-        lastScrollTop = st;
-        }
-        //스크롤 기능 끝
             $('.menu li').show();
         }
     });
-
 
     // width가 1023이하 일때
     $(window).on('resize load', function(){
@@ -81,6 +83,5 @@ $(document).ready(function(){
 
     });
 
+
 });
-
-
