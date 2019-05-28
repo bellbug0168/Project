279bc1141449ed3cea353c87bacbc7a4b5a445be
myojin <myooojiiin@naver.com>
2019-05-28 10:24:02 +0900

    김묘진 1페이지 추가합니다.

--- js/slide_banner.js
+++ js/slide_banner.js
@@ -1,72 +1,18 @@
 $(document).ready(function () {
-    var visual = $('#brandVisual > ul > li');
-    var button = $('#buttonList > li');
 
-    var current = 0;
-    var setIntervalId;
-
-   /* button.on({
-        click: function () {
-            var tg = $(this);
-            var i = tg.index();
-
-            button.removeClass('on');
-            tg.addClass('on');
-            move(i);
-        }
-    });*/
-
-    button.on('click keyup',function(){
-         var tg = $(this);
-            var i = tg.index();
-
-            button.removeClass('on');
-            tg.addClass('on');
-            move(i);
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: '0'
+        }, 600);
     });
 
-    /**/
-    $('#wrap').on({
-        mouseover: function () {
-            clearInterval(setIntervalId);
-        },
-        mouseout: function () {
-            timer();
-        }
+    $(".center").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
     });
 
-    timer();
 
-    function timer() {
-        setIntervalId = setInterval(function () {
-            var n = current + 1;
-            if (n == visual.size()) {
-                n = 0;
-            }
-            button.eq(n).trigger('click');
-        }, 500);
-
-    }
-    function move(i) {
-        if (current == i) return;
-
-        var currentEl = visual.eq(current);
-        var nextEl = visual.eq(i);
-
-        currentEl.css({
-            left: 0
-        }).stop().animate({
-            left: '-100%'
-        });
-        nextEl.css({
-            left: '100%'
-        }).stop().animate({
-            left: 0
-        });
-        current = i;
-    }
-
-    $('#brandHover').on('mouseover',function(){
-        $(this).slideDown();
-    });
 });
